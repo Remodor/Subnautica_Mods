655f3f562435c539a5c4b4178680d56c49e51a16
Remodor <remodoor@gmail.com>
2020-07-07 01:07:10 +0200

    FastBuild: v1.1 progressive mode

--- Rm_FastBuild/src/configuration/BuildConfig.cs
+++ Rm_FastBuild/src/configuration/BuildConfig.cs
@@ -6,10 +6,16 @@ namespace Rm_FastBuild
     public class BuildConfig
     {
         public float BuildingSpeed = 1f;
+        public float MaximumBuildTime = 6f;
+        public float MinimumBuildTime = 2f;
+        public bool Progressive = true;
 
         public void ApplyModifier()
         {
-            BuildSettings.buildingSpeed = BuildingSpeed;
+            BuildSettings.buildingSpeed = Mathf.Max(BuildingSpeed, 0.1f);
+            BuildSettings.maximumBuildTime = Mathf.Max(MaximumBuildTime, 0.1f, MinimumBuildTime + 0.1f);
+            BuildSettings.minimumBuildTime = Mathf.Clamp(MinimumBuildTime, 0.1f, MaximumBuildTime);
+            BuildSettings.progressive = Progressive;
         }
     }
 }
--- Rm_FastBuild/src/configuration/Options.cs
+++ Rm_FastBuild/src/configuration/Options.cs
@@ -11,14 +11,33 @@ namespace Rm_FastBuild
         public Options() : base("Fast Build")
         {
             SliderChanged += Options_SliderChanged;
+            ToggleChanged += Options_ToggleChanged;
             GameObjectCreated += Options_GameObjectCreated;
         }
+
+        public void Options_ToggleChanged(object sender, ToggleChangedEventArgs e)
+        {
+            if (e.Id == "Progressive")
+            {
+                Config<BuildConfig>.Get().Progressive = e.Value;
+                Changed();
+            }
+        }
+
         public void Options_SliderChanged(object sender, SliderChangedEventArgs e)
         {
             switch (e.Id)
             {
                 case "BuildingSpeed":
                     Config<BuildConfig>.Get().BuildingSpeed = e.Value;
+                    Changed();
+                    return;
+                case "MaximumBuildTime":
+                    Config<BuildConfig>.Get().MaximumBuildTime = e.Value;
+                    Changed();
+                    return;
+                case "MinimumBuildTime":
+                    Config<BuildConfig>.Get().MinimumBuildTime = e.Value;
                     Changed();
                     return;
             }
@@ -42,6 +61,12 @@ namespace Rm_FastBuild
                 slider.AddComponent<StepSlider_001>();
                 return;
             }
+            if (e.Id == "MaximumBuildTime" || e.Id == "MinimumBuildTime")
+            {
+                GameObject slider = gameObject.transform.Find("Slider").gameObject;
+                slider.AddComponent<StepSlider_01>();
+                return;
+            }
         }
         private class StepSlider_001 : ModSliderOption.SliderValue
         {
@@ -51,9 +76,20 @@ namespace Rm_FastBuild
                 base.UpdateLabel();
             }
         }
+        private class StepSlider_01 : ModSliderOption.SliderValue
+        {
+            protected override void UpdateLabel()
+            {
+                slider.value = Mathf.Round(slider.value * 10) / 10;
+                base.UpdateLabel();
+            }
+        }
         public override void BuildModOptions()
         {
-            AddSliderOption("BuildingSpeed",         "Building speed", 0.1f, 2f, Config<BuildConfig>.Get().BuildingSpeed, 1f);
+            AddSliderOption("BuildingSpeed", "Building speed", 0.1f, 2f, Config<BuildConfig>.Get().BuildingSpeed, 1f);
+            AddSliderOption("MaximumBuildTime", "Maximum Building Time", 0.1f, 10f, Config<BuildConfig>.Get().MaximumBuildTime, 10f);
+            AddSliderOption("MinimumBuildTime", "Minimum Building Time", 0.1f, 10f, Config<BuildConfig>.Get().MinimumBuildTime, 1f);
+            AddToggleOption("Progressive", "Progressive Building", Config<BuildConfig>.Get().Progressive);
             constructed = true;
         }
     }
