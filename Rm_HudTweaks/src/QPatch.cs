c66fc934915f72f0e83c179b45cdbe3ee18a98cc
Remodor <remodoor@gmail.com>
2022-06-04 10:55:23 +0200

    Add HudTweaks

---
+++ Rm_HudTweaks/src/QPatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System;
+using HarmonyLib;
+using Rm_Config;
+using SMLHelper.V2.Handlers;
+using QModManager.API.ModLoading;
+
+namespace Rm_HudTweaks
+{
+    [QModCore]
+    public static class QPatch 
+    {
+        [QModPatch]
+        public static void Patch() 
+        {
+            Console.WriteLine("[Rm_HudTweaks] Begin patching");
+            Config<HudTweaksConfig>.LoadConfiguration();
+            OptionsPanelHandler.RegisterModOptions(new Options());
+            var assembly = Assembly.GetExecutingAssembly();
+            new Harmony($"Remodor_{assembly.GetName().Name}").PatchAll(assembly);
+            Console.WriteLine("[Rm_HudTweaks] Successfully patched!");
+        }
+    }
+}
---
+++ Rm_HudTweaks/src/configuration/HudTweaksConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace Rm_HudTweaks
+{
+    public class HudTweaksConfig
+    {
+        public bool HideNames = false;
+        public bool DisableText2 = false;
+        public bool HideToolControlsText = false;
+        public bool HideToolInfo = false;
+        public bool DisableInputIcon = false;
+        public float CrosshairScale = 1f;
+        public bool DisableCrosshair = false;
+        public bool ShowCrosshairInteractIcons = false;
+        public bool AllowCrosshairCyclops = false;
+        public int CyclopsCrosshairIcon = 1;
+        public bool AllowCrosshairCyclopsOnlyOnHover = false;
+        public float CyclopsSpeedCrosshairLingerTime = 0.5f;
+    }
+}
