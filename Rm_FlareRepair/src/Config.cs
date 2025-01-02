a234546c845f7f1767dd3ccbd47000089488c4f0
Remodor <remodoor@gmail.com>
2025-01-02 16:15:49 +0100

    refactor: add OnConfigChange FlareRepair & HudTweaks

--- Rm_FlareRepair/src/Config.cs
+++ Rm_FlareRepair/src/Config.cs
@@ -6,19 +6,19 @@ namespace Rm_FlareRepair;
 [Menu("Flare Repair Settings")]
 public class Config : ConfigFile
 {
-    [Slider("Total Duration (Game Days)", 0.01f, 2.0f, DefaultValue = 0.5f, Format = "{0:F2}", Step = 0.01f)]
+    [Slider("Total Duration (Game Days)", 0.01f, 2.0f, DefaultValue = 0.5f, Format = "{0:F2}", Step = 0.01f), OnChange("OnConfigChange")]
     public float TotalDuration = 0.5f;
 
-    [Slider("Intensity Modifier", 0.01f, 2.0f, DefaultValue = 1.0f, Format = "{0:F2}", Step = 0.01f)]
+    [Slider("Intensity Modifier", 0.01f, 2.0f, DefaultValue = 1.0f, Format = "{0:F2}", Step = 0.01f), OnChange("OnConfigChange")]
     public float IntensityModifier = 1.0f;
 
-    [Slider("Progressive Dimming Threshold", 0.0f, 1.0f, DefaultValue = 0.5f, Format = "{0:F2}", Step = 0.01f, Tooltip = "Setting this value >0 will progressively dim the flare intensity starting at the set threshold and used 'Intensity Modifier'. For example, a value of 0.5 will start dimming the flare when 50% of the time is left.")]
+    [Slider("Progressive Dimming Threshold", 0.0f, 1.0f, DefaultValue = 0.5f, Format = "{0:F2}", Step = 0.01f, Tooltip = "Setting this value >0 will progressively dim the flare intensity starting at the set threshold and used 'Intensity Modifier'. For example, a value of 0.5 will start dimming the flare when 50% of the time is left."), OnChange("OnConfigChange")]
     public float ProgressiveDimmingThreshold = 0.5f;
 
-    [Slider("Minimum Relative Factor", 0.0f, 0.99f, DefaultValue = 0.2f, Format = "{0:F2}", Step = 0.01f, Tooltip = "Only enabled if 'Progressive Dimming Threshold' is >0. This value sets the reached minimum intensity when the flare is depleted and progressive dimming is used. For example, a value of 0.25 will result in a flare intensity of 25% of the 'Intensity Modifier' at the end of the flare's lifetime.")]
+    [Slider("Minimum Relative Factor", 0.0f, 0.99f, DefaultValue = 0.2f, Format = "{0:F2}", Step = 0.01f, Tooltip = "Only enabled if 'Progressive Dimming Threshold' is >0. This value sets the reached minimum intensity when the flare is depleted and progressive dimming is used. For example, a value of 0.25 will result in a flare intensity of 25% of the 'Intensity Modifier' at the end of the flare's lifetime."), OnChange("OnConfigChange")]
     public float MinimumRelativeFactor = 0.2f;
 
-    [Toggle("Keep Depleted Flares", Tooltip = "Enabling this option keeps depleted flares. They will persist when saving. Disabling this option will remove all kept flares as long as they are currently loaded.")]
+    [Toggle("Keep Depleted Flares", Tooltip = "Enabling this option keeps depleted flares. They will persist when saving. Disabling this option will remove all kept flares as long as they are currently loaded."), OnChange("OnConfigChange")]
     public bool KeepDepletedFlare = true;
 
     private static Config Instance;
@@ -26,10 +26,20 @@ public class Config : ConfigFile
     public static void RegisterConfig()
     {
         Instance = OptionsPanelHandler.RegisterModOptions<Config>();
+        OnConfigChange();
     }
-    public static float TotalDuration_ => Instance.TotalDuration * 1200;
-    public static float IntensityModifier_ => Instance.IntensityModifier;
-    public static bool KeepDepletedFlare_ => Instance.KeepDepletedFlare;
-    public static float ProgressiveDimmingThreshold_ => Instance.ProgressiveDimmingThreshold;
-    public static float MinimumRelativeFactor_ => Instance.MinimumRelativeFactor;
+    public static void OnConfigChange()
+    {
+        TotalDuration_ = Instance.TotalDuration * 1200;
+        IntensityModifier_ = Instance.IntensityModifier;
+        KeepDepletedFlare_ = Instance.KeepDepletedFlare;
+        ProgressiveDimmingThreshold_ = Instance.ProgressiveDimmingThreshold;
+        MinimumRelativeFactor_ = Instance.MinimumRelativeFactor;
+    }
+
+    internal static float TotalDuration_;
+    internal static float IntensityModifier_;
+    internal static bool KeepDepletedFlare_;
+    internal static float ProgressiveDimmingThreshold_;
+    internal static float MinimumRelativeFactor_;
 }
--- Rm_HudTweaks/src/Config.cs
+++ Rm_HudTweaks/src/Config.cs
@@ -7,55 +7,77 @@ public enum CyclopsCrosshair { Hidden, Always, OnlyOnHover }
 [Menu("Hud Tweak Settings")]
 public class Config : ConfigFile
 {
-    [Toggle("Hide Names", Tooltip = "Hide the entity name text. Like 'Peeper', when you aim at a peeper.")]
+    [Toggle("Hide Names", Tooltip = "Hide the entity name text. Like 'Peeper', when you aim at a peeper."), OnChange("OnConfigChange")]
     public bool HideNames = true;
 
-    [Toggle("Hide Text 2", Tooltip = "Hide additional texts, like the 'Deconstruct'-text when using the builder or the 'empty'-text when aiming at a locker.")]
+    [Toggle("Hide Text 2", Tooltip = "Hide additional texts, like the 'Deconstruct'-text when using the builder or the 'empty'-text when aiming at a locker."), OnChange("OnConfigChange")]
     public bool HideText2 = true;
-    [Toggle("Hide Input Icon", Tooltip = "Hide the input icon, e.g. (<color=#ADF8FFFF></color>) when opening a locker.")]
 
+    [Toggle("Hide Input Icon", Tooltip = "Hide the input icon, e.g. (<color=#ADF8FFFF></color>) when opening a locker."), OnChange("OnConfigChange")]
     public bool HideInputIcon = true;
 
-    [Toggle("Hide Tool Controls", Tooltip = "Hide tool controls. Also affects Seamoth and Prawn Suit texts.")]
+    [Toggle("Hide Tool Controls", Tooltip = "Hide tool controls. Also affects Seamoth and Prawn Suit texts."), OnChange("OnConfigChange")]
     public bool HideToolControlsText = true;
 
-    [Toggle("Hide Tool Info", Tooltip = "Hide tool infos, like the battery status. Also affects Seamoth and Prawn Suit texts.")]
+    [Toggle("Hide Tool Info", Tooltip = "Hide tool infos, like the battery status. Also affects Seamoth and Prawn Suit texts."), OnChange("OnConfigChange")]
     public bool HideToolInfo = true;
 
-    [Slider("Crosshair Scale", 0.01f, 2.0f, DefaultValue = 1.0f, Format = "{0:F2}", Step = 0.01f, Tooltip = "Change the crosshair scale.")]
+    [Slider("Crosshair Scale", 0.01f, 2.0f, DefaultValue = 1.0f, Format = "{0:F2}", Step = 0.01f, Tooltip = "Change the crosshair scale."), OnChange("OnConfigChange")]
     public float CrosshairScale = 1.0f;
 
-    [Toggle("Hide Crosshair", Tooltip = "Hide the default crosshair. Other interact icons are not affected.")]
+    [Toggle("Hide Crosshair", Tooltip = "Hide the default crosshair. Other interact icons are not affected."), OnChange("OnConfigChange")]
     public bool HideCrosshair = true;
 
-    [Toggle("Hide Scuba Mask", Tooltip = "Hide the scuba mask when diving. Applies when entering the water.")]
+    [Toggle("Hide Scuba Mask", Tooltip = "Hide the scuba mask when diving. Applies when entering the water."), OnChange("OnConfigChange")]
     public bool HideScubaMask = false;
 
-    [Toggle("Hide Interact Icons", Tooltip = "Hide the interact icons, e.g. when entering a sub. Depending on the 'Hide Crosshair'-setting, either the default crosshair is shown or nothing if it is hidden.")]
+    [Toggle("Hide Interact Icons", Tooltip = "Hide the interact icons, e.g. when entering a sub. Depending on the 'Hide Crosshair'-setting, either the default crosshair is shown or nothing if it is hidden."), OnChange("OnConfigChange")]
     public bool HideInteractIcons = true;
-    [Choice("Cyclops Crosshair", Tooltip = "Show a crosshair when piloting the cyclops. Overrides a hidden crosshair. 'OnlyOnHover': Only shows the crosshair when hovering cyclops control elements.\n(Has additional options: 'Crosshair Icon', 'Speed Linger Time')")]
+
+    [Choice("Cyclops Crosshair", Tooltip = "Show a crosshair when piloting the cyclops. Overrides a hidden crosshair. 'OnlyOnHover': Only shows the crosshair when hovering cyclops control elements.\n(Has additional options: 'Crosshair Icon', 'Speed Linger Time')"), OnChange("OnConfigChange")]
     public CyclopsCrosshair CyclopsCrosshair = CyclopsCrosshair.OnlyOnHover;
-    [Slider("    Crosshair Icon", 1, 10, DefaultValue = 1, Tooltip = "Change the cyclops crosshair icon.")]
+
+    [Slider("    Crosshair Icon", 1, 10, DefaultValue = 1, Tooltip = "Change the cyclops crosshair icon."), OnChange("OnConfigChange")]
     public int CyclopsCrosshairIcon = 1;
-    [Slider("    Speed Linger Time", 0.1f, 3.0f, DefaultValue = 0.5f, Format = "{0:F1}", Step = 0.1f, Tooltip = "The cyclops speed buttons required a workaround - the crosshair lingers instead of the on-and-of behavior of the other buttons. This setting controls the linger time in seconds.")]
+
+    [Slider("    Speed Linger Time", 0.1f, 3.0f, DefaultValue = 0.5f, Format = "{0:F1}", Step = 0.1f, Tooltip = "The cyclops speed buttons required a workaround - the crosshair lingers instead of the on-and-of behavior of the other buttons. This setting controls the linger time in seconds."), OnChange("OnConfigChange")]
     public float CyclopsSpeedLingerTime = 0.5f;
+
     private static Config Instance;
     public Config() : base() { }
     public static void RegisterConfig()
     {
         Instance = OptionsPanelHandler.RegisterModOptions<Config>();
+        OnConfigChange();
     }
-    public static bool HideNames_ => Instance.HideNames;
-    public static bool HideText2_ => Instance.HideText2;
-    public static bool HideToolControlsText_ => Instance.HideToolControlsText;
-    public static bool HideToolInfo_ => Instance.HideToolInfo;
-    public static bool HideInputIcon_ => Instance.HideInputIcon;
-    public static float CrosshairScale_ => Instance.CrosshairScale;
-    public static bool HideCrosshair_ => Instance.HideCrosshair;
-    public static bool HideInteractIcons_ => Instance.HideInteractIcons;
-    public static bool CyclopsCrosshairAlways_ => Instance.CyclopsCrosshair == CyclopsCrosshair.Always;
-    public static bool CyclopsCrosshairOnlyOnHover_ => Instance.CyclopsCrosshair == CyclopsCrosshair.OnlyOnHover;
-    public static int CyclopsCrosshairIcon_ => Instance.CyclopsCrosshairIcon;
-    public static float CyclopsSpeedLingerTime_ => Instance.CyclopsSpeedLingerTime;
-    public static bool HideScubaMask_ => Instance.HideScubaMask;
+    public static void OnConfigChange()
+    {
+        HideNames_ = Instance.HideNames;
+        HideText2_ = Instance.HideText2;
+        HideToolControlsText_ = Instance.HideToolControlsText;
+        HideToolInfo_ = Instance.HideToolInfo;
+        HideInputIcon_ = Instance.HideInputIcon;
+        CrosshairScale_ = Instance.CrosshairScale;
+        HideCrosshair_ = Instance.HideCrosshair;
+        HideInteractIcons_ = Instance.HideInteractIcons;
+        CyclopsCrosshairAlways_ = Instance.CyclopsCrosshair == CyclopsCrosshair.Always;
+        CyclopsCrosshairOnlyOnHover_ = Instance.CyclopsCrosshair == CyclopsCrosshair.OnlyOnHover;
+        CyclopsCrosshairIcon_ = Instance.CyclopsCrosshairIcon;
+        CyclopsSpeedLingerTime_ = Instance.CyclopsSpeedLingerTime;
+        HideScubaMask_ = Instance.HideScubaMask;
+    }
+
+    internal static bool HideNames_;
+    internal static bool HideText2_;
+    internal static bool HideToolControlsText_;
+    internal static bool HideToolInfo_;
+    internal static bool HideInputIcon_;
+    internal static float CrosshairScale_;
+    internal static bool HideCrosshair_;
+    internal static bool HideInteractIcons_;
+    internal static bool CyclopsCrosshairAlways_;
+    internal static bool CyclopsCrosshairOnlyOnHover_;
+    internal static int CyclopsCrosshairIcon_;
+    internal static float CyclopsSpeedLingerTime_;
+    internal static bool HideScubaMask_;
 }
